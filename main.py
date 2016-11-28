31911ca6fbc75baed1cd2f4e3d3bd9540781b8cf
Aniello Maiese <aniello.maiese@mecglobal.com>
2016-11-28 22:23:50 +0100

    Catched failed upload and save it to json

--- main.py
+++ main.py
@@ -1,29 +1,47 @@
 import neo4jconnection
 import readBz2
 import chunkfile
+import json
 from tqdm import tqdm
 
 #Original DB file
 filename = '/Users/N/Desktop/MEC/roadtorome/rawfile/itwiki-20161120-pages-articles.xml.bz2'
 #Folder for splitted DB files
-folder = 'data'
+folder = 'chunks'
 
 
 if __name__ == '__main__':
 
     # Split Big file in smaller files
+
+    print ' ########### Chunk file #############'
     chunkfile.split_xml(filename, folder)
 
+    print ' ########### Read all file #############'
     # Get all files in folder
     files = readBz2.findFiles(folder, '*.bz2')
     graph_list = []
 
+    print ' ########### Generate Graph Object #############'
     # For every file, extract articles list and create object graph to upload
     for file in tqdm(files):
         articles = readBz2.extractBZ2Articles(file)
-    graph_list += readBz2.extractTarget(articles)
-    # Upload a graph object to db
-    for g in graph_list:
-        neo4jconnection.addObjectGraph(g)
+        graph_list += readBz2.extractTarget(articles)
 
+
+    failed = []
+
+    print ' ########### Upload to DB #############'
+    # Upload a graph object to db
+    for g in tqdm(graph_list):
+        try:
+            neo4jconnection.addObjectGraph(g)
+        except:
+            print '\n FAILED --> ', g
+            failed.append(g)
+
+
+    with open('failed.json', 'w') as f:
+        json.dump(failed, f)
+    
 
--- neo4jconnection.py
+++ neo4jconnection.py
@@ -1,5 +1,5 @@
 from neo4j.v1 import GraphDatabase, basic_auth
-
+import re
 
 def openNeo4j():
     '''Open connection to Neo4j DB'''
@@ -11,13 +11,17 @@ def addObjectGraph(obj):
     '''for a single dict create the nodes for all element and the connections between them'''
     session = openNeo4j()
     source = ''.join(e for e in obj['key'] if e.isalnum())
+    title = re.sub('[^a-zA-Z0-9 \n\.]', '', obj['key'])
+
     #Add node
-    session.run("MERGE (`"+source+"`:Article {title:'"+obj['key'].replace("'", "")+"'});")
+    session.run("MERGE (`"+source+"`:Article {title:'"+title+"'});")
     for s in obj['target']:
         target = ''.join(e for e in s if e.isalnum())
-        session.run("MERGE (`" + target + "`:Article {title:'" + s.replace("'", "") + "'});")
-        session.run("MATCH (a:Article),(b:Article) WHERE a.title = '"+obj['key'].replace("'", "")+"'"
-                    "and b.title = '"+s.replace("'","")+"' MERGE (a)-[LINK:LINK]->(b);")
+        ttitle = re.sub('[^a-zA-Z0-9 \n\.]', '', s)
+        session.run("MERGE (`" + target + "`:Article {title:'" + ttitle+ "'});")
+        session.run("MATCH (a:Article),(b:Article) WHERE a.title = '"+title+"'"
+                    "and b.title = '"+ttitle+"' MERGE (a)-[LINK:LINK]->(b);")
+
     session.close()
 
 
